b8dffd1a20cbe726f38f44d2225b9cfcb70a9a54
marshalonah <marshalonah@gmail.com>
2022-02-03 12:48:12 +0100

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.11;
+
+contract Election {
+    //store candidate
+    //read candidate
+    string public candidate;
+
+    constructor(){
+        candidate = "Candidate 1";
+    }
+}
